c61d9fb18e4c2885feb5c7f6525a900820e9dc35
Hoang Dinh <dvh312@gmail.com>
2016-07-05 19:02:45 +1000

    add emoticons array (JSON) in replace.js

--- js/replace.js
+++ js/replace.js
@@ -471,3 +471,357 @@ const keyComb = [
 	":-)/\\:-)",
 	"[]==[]",
 ];
+
+
+var emoticons = [{
+	"key": [":)"],
+	"src": "images/YahooEmoticons/1.gif"
+}, {
+	"key": [":("],
+	"src": "images/YahooEmoticons/2.gif"
+}, {
+	"key": [";)"],
+	"src": "images/YahooEmoticons/3.gif"
+}, {
+	"key": [":D"],
+	"src": "images/YahooEmoticons/4.gif"
+}, {
+	"key": [";;)"],
+	"src": "images/YahooEmoticons/5.gif"
+}, {
+	"key": [">:D<"],
+	"src": "images/YahooEmoticons/6.gif"
+}, {
+	"key": [":-/"],
+	"src": "images/YahooEmoticons/7.gif"
+}, {
+	"key": [":x"],
+	"src": "images/YahooEmoticons/8.gif"
+}, {
+	"key": [":\">"],
+	"src": "images/YahooEmoticons/9.gif"
+}, {
+	"key": [":P"],
+	"src": "images/YahooEmoticons/10.gif"
+}, {
+	"key": [":-*"],
+	"src": "images/YahooEmoticons/11.gif"
+}, {
+	"key": ["=(("],
+	"src": "images/YahooEmoticons/12.gif"
+}, {
+	"key": [":-O"],
+	"src": "images/YahooEmoticons/13.gif"
+}, {
+	"key": ["X("],
+	"src": "images/YahooEmoticons/14.gif"
+}, {
+	"key": [":>"],
+	"src": "images/YahooEmoticons/15.gif"
+}, {
+	"key": ["B-)"],
+	"src": "images/YahooEmoticons/16.gif"
+}, {
+	"key": [":-S"],
+	"src": "images/YahooEmoticons/17.gif"
+}, {
+	"key": ["#:-S"],
+	"src": "images/YahooEmoticons/18.gif"
+}, {
+	"key": [">:)"],
+	"src": "images/YahooEmoticons/19.gif"
+}, {
+	"key": [":(("],
+	"src": "images/YahooEmoticons/20.gif"
+}, {
+	"key": [":))"],
+	"src": "images/YahooEmoticons/21.gif"
+}, {
+	"key": [":|"],
+	"src": "images/YahooEmoticons/22.gif"
+}, {
+	"key": ["/:)"],
+	"src": "images/YahooEmoticons/23.gif"
+}, {
+	"key": ["=))"],
+	"src": "images/YahooEmoticons/24.gif"
+}, {
+	"key": ["O:-)"],
+	"src": "images/YahooEmoticons/25.gif"
+}, {
+	"key": [":-B"],
+	"src": "images/YahooEmoticons/26.gif"
+}, {
+	"key": ["=;"],
+	"src": "images/YahooEmoticons/27.gif"
+}, {
+	"key": ["I-)"],
+	"src": "images/YahooEmoticons/28.gif"
+}, {
+	"key": ["8-|"],
+	"src": "images/YahooEmoticons/29.gif"
+}, {
+	"key": ["L-)"],
+	"src": "images/YahooEmoticons/30.gif"
+}, {
+	"key": [":-&"],
+	"src": "images/YahooEmoticons/31.gif"
+}, {
+	"key": [":-$"],
+	"src": "images/YahooEmoticons/32.gif"
+}, {
+	"key": ["[-("],
+	"src": "images/YahooEmoticons/33.gif"
+}, {
+	"key": [":O)"],
+	"src": "images/YahooEmoticons/34.gif"
+}, {
+	"key": ["8-}"],
+	"src": "images/YahooEmoticons/35.gif"
+}, {
+	"key": ["<:-P"],
+	"src": "images/YahooEmoticons/36.gif"
+}, {
+	"key": ["(:|"],
+	"src": "images/YahooEmoticons/37.gif"
+}, {
+	"key": ["=P~"],
+	"src": "images/YahooEmoticons/38.gif"
+}, {
+	"key": [":-?"],
+	"src": "images/YahooEmoticons/39.gif"
+}, {
+	"key": ["#-o"],
+	"src": "images/YahooEmoticons/40.gif"
+}, {
+	"key": ["=D>"],
+	"src": "images/YahooEmoticons/41.gif"
+}, {
+	"key": [":-SS"],
+	"src": "images/YahooEmoticons/42.gif"
+}, {
+	"key": ["@-)"],
+	"src": "images/YahooEmoticons/43.gif"
+}, {
+	"key": [":^o"],
+	"src": "images/YahooEmoticons/44.gif"
+}, {
+	"key": [":-w"],
+	"src": "images/YahooEmoticons/45.gif"
+}, {
+	"key": [":-<"],
+	"src": "images/YahooEmoticons/46.gif"
+}, {
+	"key": [">:P"],
+	"src": "images/YahooEmoticons/47.gif"
+}, {
+	"key": ["<):)"],
+	"src": "images/YahooEmoticons/48.gif"
+}, {
+	"key": [":@)"],
+	"src": "images/YahooEmoticons/49.gif"
+}, {
+	"key": ["3:-O"],
+	"src": "images/YahooEmoticons/50.gif"
+}, {
+	"key": [":(|)"],
+	"src": "images/YahooEmoticons/51.gif"
+}, {
+	"key": ["~:>"],
+	"src": "images/YahooEmoticons/52.gif"
+}, {
+	"key": ["@};-"],
+	"src": "images/YahooEmoticons/53.gif"
+}, {
+	"key": ["%%-"],
+	"src": "images/YahooEmoticons/54.gif"
+}, {
+	"key": ["**=="],
+	"src": "images/YahooEmoticons/55.gif"
+}, {
+	"key": ["(~~)"],
+	"src": "images/YahooEmoticons/56.gif"
+}, {
+	"key": ["~O)"],
+	"src": "images/YahooEmoticons/57.gif"
+}, {
+	"key": ["*-:)"],
+	"src": "images/YahooEmoticons/58.gif"
+}, {
+	"key": ["8-X"],
+	"src": "images/YahooEmoticons/59.gif"
+}, {
+	"key": ["=:)"],
+	"src": "images/YahooEmoticons/60.gif"
+}, {
+	"key": [">-)"],
+	"src": "images/YahooEmoticons/61.gif"
+}, {
+	"key": [":-L"],
+	"src": "images/YahooEmoticons/62.gif"
+}, {
+	"key": ["[-O<"],
+	"src": "images/YahooEmoticons/63.gif"
+}, {
+	"key": ["$-)"],
+	"src": "images/YahooEmoticons/64.gif"
+}, {
+	"key": [":-\""],
+	"src": "images/YahooEmoticons/65.gif"
+}, {
+	"key": ["b-("],
+	"src": "images/YahooEmoticons/66.gif"
+}, {
+	"key": [":)>-"],
+	"src": "images/YahooEmoticons/67.gif"
+}, {
+	"key": ["[-X"],
+	"src": "images/YahooEmoticons/68.gif"
+}, {
+	"key": ["\\:D/"],
+	"src": "images/YahooEmoticons/69.gif"
+}, {
+	"key": [">:/"],
+	"src": "images/YahooEmoticons/70.gif"
+}, {
+	"key": [";))"],
+	"src": "images/YahooEmoticons/71.gif"
+}, {
+	"key": ["o->"],
+	"src": "images/YahooEmoticons/72.gif"
+}, {
+	"key": ["o=>"],
+	"src": "images/YahooEmoticons/73.gif"
+}, {
+	"key": ["o-+"],
+	"src": "images/YahooEmoticons/74.gif"
+}, {
+	"key": ["(%)"],
+	"src": "images/YahooEmoticons/75.gif"
+}, {
+	"key": [":-@"],
+	"src": "images/YahooEmoticons/76.gif"
+}, {
+	"key": ["^:)^"],
+	"src": "images/YahooEmoticons/77.gif"
+}, {
+	"key": [":-j"],
+	"src": "images/YahooEmoticons/78.gif"
+}, {
+	"key": ["(*)"],
+	"src": "images/YahooEmoticons/79.gif"
+}, {
+	"key": [":)]"],
+	"src": "images/YahooEmoticons/100.gif"
+}, {
+	"key": [":-c"],
+	"src": "images/YahooEmoticons/101.gif"
+}, {
+	"key": ["~X("],
+	"src": "images/YahooEmoticons/102.gif"
+}, {
+	"key": [":-h"],
+	"src": "images/YahooEmoticons/103.gif"
+}, {
+	"key": [":-t"],
+	"src": "images/YahooEmoticons/104.gif"
+}, {
+	"key": ["8->"],
+	"src": "images/YahooEmoticons/105.gif"
+}, {
+	"key": [":-??"],
+	"src": "images/YahooEmoticons/106.gif"
+}, {
+	"key": ["%-("],
+	"src": "images/YahooEmoticons/107.gif"
+}, {
+	"key": [":o3"],
+	"src": "images/YahooEmoticons/108.gif"
+}, {
+	"key": ["X_X"],
+	"src": "images/YahooEmoticons/109.gif"
+}, {
+	"key": [":!!"],
+	"src": "images/YahooEmoticons/110.gif"
+}, {
+	"key": ["\\m/"],
+	"src": "images/YahooEmoticons/111.gif"
+}, {
+	"key": [":-q"],
+	"src": "images/YahooEmoticons/112.gif"
+}, {
+	"key": [":-bd"],
+	"src": "images/YahooEmoticons/113.gif"
+}, {
+	"key": ["^#(^"],
+	"src": "images/YahooEmoticons/114.gif"
+}, {
+	"key": [":bz"],
+	"src": "images/YahooEmoticons/115.gif"
+}, {
+	"key": ["~^o^~"],
+	"src": "images/YahooEmoticons/116.gif"
+}, {
+	"key": ["'@^@|||"],
+	"src": "images/YahooEmoticons/117.gif"
+}, {
+	"key": ["[]---"],
+	"src": "images/YahooEmoticons/118.gif"
+}, {
+	"key": ["^o^||3"],
+	"src": "images/YahooEmoticons/119.gif"
+}, {
+	"key": [":-(||>"],
+	"src": "images/YahooEmoticons/120.gif"
+}, {
+	"key": ["'+_+"],
+	"src": "images/YahooEmoticons/121.gif"
+}, {
+	"key": [":::^^:::"],
+	"src": "images/YahooEmoticons/122.gif"
+}, {
+	"key": ["o|^_^|o"],
+	"src": "images/YahooEmoticons/123.gif"
+}, {
+	"key": [":puke!"],
+	"src": "images/YahooEmoticons/124.gif"
+}, {
+	"key": ["o|\\~"],
+	"src": "images/YahooEmoticons/125.gif"
+}, {
+	"key": ["o|:-)"],
+	"src": "images/YahooEmoticons/126.gif"
+}, {
+	"key": [":(fight)"],
+	"src": "images/YahooEmoticons/127.gif"
+}, {
+	"key": ["%*-{"],
+	"src": "images/YahooEmoticons/128.gif"
+}, {
+	"key": ["%||:-{"],
+	"src": "images/YahooEmoticons/129.gif"
+}, {
+	"key": ["&[]"],
+	"src": "images/YahooEmoticons/130.gif"
+}, {
+	"key": [":(tv)"],
+	"src": "images/YahooEmoticons/131.gif"
+}, {
+	"key": ["?@_@?"],
+	"src": "images/YahooEmoticons/132.gif"
+}, {
+	"key": [":->~~"],
+	"src": "images/YahooEmoticons/133.gif"
+}, {
+	"key": ["'@-@"],
+	"src": "images/YahooEmoticons/134.gif"
+}, {
+	"key": [":(game)"],
+	"src": "images/YahooEmoticons/135.gif"
+}, {
+	"key": [":-)/\\:-)"],
+	"src": "images/YahooEmoticons/136.gif"
+}, {
+	"key": ["[]==[]"],
+	"src": "images/YahooEmoticons/137.gif"
+}]
