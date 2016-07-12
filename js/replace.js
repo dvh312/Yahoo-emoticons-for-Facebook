6a56457a410fb0499b6fe3af005d21c23eae4660
Hoang Dinh <dvh312@gmail.com>
2016-07-12 18:58:20 +1000

    add mutation observe for chatdock

--- js/replace.js
+++ js/replace.js
@@ -1,18 +1,38 @@
 const debugging = true; //turn print debug on or off
 var isEnabled, emoticons; //storageVariable
-
 refreshStorage();
 setTimeout(main, 1000);
 
 //FUNCTION + EVENTS
 
 function main(){
+	if (debugging){
+		var t0 = performance.now();
+	}
+
 	if (isEnabled){
 		debug("entering main");
+		var chatDock = $("#ChatTabsPagelet")[0];
+		observer.observe(chatDock, config);
+	}
 
-
+	if (debugging){
+		var t1 = performance.now();
+		debug("Call to main took " + (t1 - t0) + " milliseconds.");
 	}
 }
+
+// Create an observer instance
+var observer = new MutationObserver(function( mutations ) {
+	debug("dock changed");
+	var chatBoxs = $(".conversation", chatDock);
+	
+});
+// Configuration of the observer:
+var config = { 
+	childList: true, 
+	subtree: true
+};
 
 /**
  * refresh storage variables (local in content script)
