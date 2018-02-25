cd413e13cd932b64ab4be7371a1ae11b5257f8b4
Hoang Dinh <dvh312@gmail.com>
2018-02-25 22:36:46 +0700

    rearranged code

--- js/content.js
+++ js/content.js
@@ -12,6 +12,19 @@ class Service {
     });
   }
 
+  /**
+   * DOM changed listener.
+   */
+  addMutationObserver(callback) {
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    const observer = new MutationObserver(callback);
+    observer.observe(document, {
+      subtree: true,
+      childList: true,
+    });
+  }
+
   // Ref: https://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
   addStyleToHead(css) {
     let head = document.head || document.getElementsByTagName('head')[0];
@@ -25,19 +38,6 @@ class Service {
     }
 
     head.appendChild(style);
-  }
-
-  /**
-   * DOM changed listener.
-   */
-  addMutationObserver(callback) {
-    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-    const observer = new MutationObserver(callback);
-    observer.observe(document, {
-      subtree: true,
-      childList: true,
-    });
   }
 
   addYahooEmoticonPickerButton(categoryElement) {
@@ -67,6 +67,64 @@ class Service {
     return new Promise((resolve, reject) => {
       chrome.storage.sync.get(items => resolve(!!items.isEnabled));
     });
+  }
+
+  copyTextToClipboard(text) {
+    var textArea = document.createElement('textarea');
+
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a flash,
+    // so some of these are just precautions. However in IE the element
+    // is visible whilst the popup box asking the user for permission for
+    // the web page to copy to the clipboard.
+    //
+
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+    try {
+      let successful = document.execCommand('copy');
+      if (!successful) {
+        console.log('Copying text command was unsuccessful');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    document.body.removeChild(textArea);
   }
 
   filterElements(origin) {
@@ -130,6 +188,24 @@ class Service {
     }
     yahooButton.classList.add('_1uwz'); // Add grey background on Yahoo button.
     this.showYahooEmojiTable();
+  }
+
+  onYahooEmojiClicked(emojiElement, emojiIndex) {
+    // Closed last popup if less than 1s.
+    if (this.lastOpenedPopup && this.lastOpenedPopup.classList.contains('show')) {
+      this.lastOpenedPopup.classList.remove('show');
+    }
+
+    // Show popup and close after 1s
+    this.clipboardPatterns.push(emoticons[emojiIndex].patterns[0]);
+    this.copyTextToClipboard(this.clipboardPatterns.join(' '));
+    const popup = emojiElement.querySelector('span.popuptext');
+    popup.classList.add('show');
+    this.lastOpenedPopup = popup;
+    clearTimeout(this.popupCloseTimeout);
+    this.popupCloseTimeout = setTimeout(() => {
+      popup.classList.remove('show');
+    }, 1000);
   }
 
   /**
@@ -255,82 +331,6 @@ class Service {
     } else {
       table.style.display = null; // Show Yahoo table.
     }
-  }
-
-  onYahooEmojiClicked(emojiElement, emojiIndex) {
-    // Closed last popup if less than 1s.
-    if (this.lastOpenedPopup && this.lastOpenedPopup.classList.contains('show')) {
-      this.lastOpenedPopup.classList.remove('show');
-    }
-
-    // Show popup and close after 1s
-    this.clipboardPatterns.push(emoticons[emojiIndex].patterns[0]);
-    this.copyTextToClipboard(this.clipboardPatterns.join(' '));
-    const popup = emojiElement.querySelector('span.popuptext');
-    popup.classList.add('show');
-    this.lastOpenedPopup = popup;
-    clearTimeout(this.popupCloseTimeout);
-    this.popupCloseTimeout = setTimeout(() => {
-      popup.classList.remove('show');
-    }, 1000);
-  }
-
-  copyTextToClipboard(text) {
-    var textArea = document.createElement('textarea');
-
-    //
-    // *** This styling is an extra step which is likely not required. ***
-    //
-    // Why is it here? To ensure:
-    // 1. the element is able to have focus and selection.
-    // 2. if element was to flash render it has minimal visual impact.
-    // 3. less flakyness with selection and copying which **might** occur if
-    //    the textarea element is not visible.
-    //
-    // The likelihood is the element won't even render, not even a flash,
-    // so some of these are just precautions. However in IE the element
-    // is visible whilst the popup box asking the user for permission for
-    // the web page to copy to the clipboard.
-    //
-
-    // Place in top-left corner of screen regardless of scroll position.
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-
-    // Ensure it has a small width and height. Setting to 1px / 1em
-    // doesn't work as this gives a negative w/h on some browsers.
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-
-    // We don't need padding, reducing the size if it does flash render.
-    textArea.style.padding = 0;
-
-    // Clean up any borders.
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-
-    // Avoid flash of white box if rendered for any reason.
-    textArea.style.background = 'transparent';
-
-
-    textArea.value = text;
-
-    document.body.appendChild(textArea);
-
-    textArea.select();
-
-    try {
-      let successful = document.execCommand('copy');
-      if (!successful) {
-        console.log('Copying text command was unsuccessful');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-
-    document.body.removeChild(textArea);
   }
 
   /**
